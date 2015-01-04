9a5729fce38a30ee478bd0c706123c9cf678a7d9
Cornelius Lilge <cornelius@wasis.nu>
2015-01-04 23:07:57 +0100

    Minor tweak to use java 8 streams

--- src/main/java/nu/wasis/jdocstat/util/MultiStringUtil.java
+++ src/main/java/nu/wasis/jdocstat/util/MultiStringUtil.java
@@ -1,14 +1,11 @@
 package nu.wasis.jdocstat.util;
 
+import java.util.Arrays;
+
 public class MultiStringUtil {
 
     public static boolean containsAny(final String str, final String... searchStrings) {
-        for (final String string : searchStrings) {
-            if (str.contains(string)) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(searchStrings).anyMatch(s -> str.contains(s));
     }
 
 }
